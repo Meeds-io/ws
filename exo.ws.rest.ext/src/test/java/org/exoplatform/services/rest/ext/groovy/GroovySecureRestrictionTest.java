6026fd67104cfcbe313714888174145832b55351
Andrey Parfonov <andrey.parfonov@exoplatform.com>
2010-06-21 10:19:10 +0000

    EXOJCR-793 : test for security restriction

---
+++ exo.ws.rest.ext/src/test/java/org/exoplatform/services/rest/ext/groovy/GroovySecureRestrictionTest.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (C) 2010 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.exoplatform.services.rest.ext.groovy;
+
+import org.exoplatform.services.rest.ext.BaseTest;
+import org.exoplatform.services.rest.impl.ContainerResponse;
+import org.exoplatform.services.rest.tools.ByteArrayContainerResponseWriter;
+
+import java.io.InputStream;
+
+/**
+ * @author <a href="mailto:andrew00x@gmail.com">Andrey Parfonov</a>
+ * @version $Id$
+ */
+public class GroovySecureRestrictionTest extends BaseTest
+{
+
+   private InputStream script;
+
+   @Override
+   public void setUp() throws Exception
+   {
+      super.setUp();
+      assertNotNull("SecurityManager not installed", System.getSecurityManager());
+      script = Thread.currentThread().getContextClassLoader().getResourceAsStream("groovy3.groovy");
+      assertNotNull(script);
+   }
+
+   public void testReadSystemPropertyFail() throws Exception
+   {
+      groovyPublisher.publishPerRequest(script, new BaseResourceId("g1"));
+      ByteArrayContainerResponseWriter writer = new ByteArrayContainerResponseWriter();
+      ContainerResponse resp = service("GET", "/a/b", "", null, null, writer);
+      assertEquals(500, resp.getStatus());
+      assertTrue(new String(writer.getBody()).startsWith("access denied"));
+   }
+
+}
