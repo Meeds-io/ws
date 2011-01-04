ee600aaf72993ce2981aad0877a8b3a37e18fcea
Dmitry Kuleshov <dmitry.kuleshov@exoplatform.com>
2011-01-04 12:47:36 +0000

    WS-258: removed some unnecessary jars

--- packaging/module/src/main/javascript/ws.packaging.module.js
+++ packaging/module/src/main/javascript/ws.packaging.module.js
@@ -41,9 +41,9 @@ function getModule(params) {
     addDependency(new Project("commons-chain", "commons-chain", "jar", "1.0")) .
 //    addDependency(new Project("javax.xml.parsers", "jaxp-api", "jar", "1.4")) .
     addDependency(new Project("javax.xml.bind", "jaxb-api", "jar", "2.1")) .
-    addDependency(new Project("com.sun.xml.bind", "jaxb-impl", "jar", "2.1.7")) .
+  //addDependency(new Project("com.sun.xml.bind", "jaxb-impl", "jar", "2.1.7")) .
 //    addDependency(new Project("com.sun.xml.parsers", "jaxp-ri", "jar", "1.4")) .
-    addDependency(new Project("org.jvnet.jaxb2.maven2", "maven-jaxb2-plugin", "jar", "0.1"));
+  //addDependency(new Project("org.jvnet.jaxb2.maven2", "maven-jaxb2-plugin", "jar", "0.1"));
           
   module.soap = {};
   module.soap.jsr181 =
@@ -51,7 +51,7 @@ function getModule(params) {
     addDependency(new Project("picocontainer", "picocontainer", "jar", "1.1")) .
     addDependency(new Project("org.codehaus.xfire", "xfire-jsr181-api", "jar", "1.0")) .
     addDependency(new Project("org.codehaus.xfire", "xfire-all", "jar", "1.2.6")) .
-    addDependency(new Project("stax", "stax-api", "jar", "1.0")) .
+ // addDependency(new Project("stax", "stax-api", "jar", "1.0")) .
     addDependency(new Project("wsdl4j", "wsdl4j", "jar", "1.6.1")) .
     addDependency(new Project("jdom", "jdom", "jar", "1.0"));  
           
