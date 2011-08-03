30ebf7c08e7d33b3aa55e20c44319481e8ec446c
Andrey Parfonov <andrey.parfonov@exoplatform.com>
2011-08-03 10:23:08 +0000

    EXOJCR-1466

--- exo.ws.frameworks.json/src/test/java/org/exoplatform/ws/frameworks/json/impl/ObjectBuilderTest.java
+++ exo.ws.frameworks.json/src/test/java/org/exoplatform/ws/frameworks/json/impl/ObjectBuilderTest.java
@@ -377,4 +377,104 @@ public class ObjectBuilderTest extends JsonTest
    public static class ForTestClass001
    {
    }
+
+   public void testConvertFromStringValue() throws Exception
+   {
+      String jsonString =
+         "{\"b\":\"1\", \"s\":\"2\" , \"i\":\"3\", \"l\":\"4\",\"f\":\"1.05\",\"d\":\"1.1\",\"bool\":\"true\"}";
+      JsonParser jsonParser = new JsonParserImpl();
+      JsonDefaultHandler handler = new JsonDefaultHandler();
+      jsonParser.parse(new ByteArrayInputStream(jsonString.getBytes()), handler);
+      JsonValue jsonValue = handler.getJsonObject();
+      //System.out.println(jsonValue);
+      ConvertFromStringValuBean o = ObjectBuilder.createObject(ConvertFromStringValuBean.class, jsonValue);
+      assertEquals(1, o.getB());
+      assertEquals(2, o.getS());
+      assertEquals(3, o.getI());
+      assertEquals(4L, o.getL());
+      assertEquals(1.05F, o.getF());
+      assertEquals(1.1D, o.getD());
+      assertEquals(true, o.isBool());
+   }
+
+   public static class ConvertFromStringValuBean
+   {
+      private byte b;
+      private short s;
+      private int i;
+      private long l;
+      private float f;
+      private double d;
+      private boolean bool;
+
+      public byte getB()
+      {
+         return b;
+      }
+
+      public void setB(byte b)
+      {
+         this.b = b;
+      }
+
+      public short getS()
+      {
+         return s;
+      }
+
+      public void setS(short s)
+      {
+         this.s = s;
+      }
+
+      public int getI()
+      {
+         return i;
+      }
+
+      public void setI(int i)
+      {
+         this.i = i;
+      }
+
+      public long getL()
+      {
+         return l;
+      }
+
+      public void setL(long l)
+      {
+         this.l = l;
+      }
+
+      public float getF()
+      {
+         return f;
+      }
+
+      public void setF(float f)
+      {
+         this.f = f;
+      }
+
+      public double getD()
+      {
+         return d;
+      }
+
+      public void setD(double d)
+      {
+         this.d = d;
+      }
+
+      public boolean isBool()
+      {
+         return bool;
+      }
+
+      public void setBool(boolean bool)
+      {
+         this.bool = bool;
+      }
+   }
 }
