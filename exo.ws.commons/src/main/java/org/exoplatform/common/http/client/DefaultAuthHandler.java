67fb74a5145db801232d277ab4730290ade45652
Trang Huyen Vu <trangvh@exoplatform.com>
2011-08-25 11:35:13 +0000

    WS-269: remove explicit call of System.gc()

--- exo.ws.commons/src/main/java/org/exoplatform/common/http/client/DefaultAuthHandler.java
+++ exo.ws.commons/src/main/java/org/exoplatform/common/http/client/DefaultAuthHandler.java
@@ -192,7 +192,6 @@ public class DefaultAuthHandler implements AuthorizationHandler, GlobalConstants
       // try to get rid of any unencoded passwords in memory
 
       answer = null;
-      System.gc();
 
       // Done
 
