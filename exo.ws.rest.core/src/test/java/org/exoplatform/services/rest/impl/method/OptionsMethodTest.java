2eaba0e3a8d50d6438455adef12f4598e3341200
Romain Dénarié <romain.denarie@exoplatform.com>
2026-01-06 15:17:02 +0100

    fix : Prevent WADL documentation to be exposed publicly - MEED-10096 - meeds-io/meeds#3959
    Before this fix, REST service exposes WADL documentation without authentication
    This fix ensure to require authentication for this call to prevent information disclosure
    
    Resolves meeds-io/meeds#3959

--- exo.ws.rest.core/src/test/java/org/exoplatform/services/rest/impl/method/OptionsMethodTest.java
+++ exo.ws.rest.core/src/test/java/org/exoplatform/services/rest/impl/method/OptionsMethodTest.java
@@ -16,16 +16,22 @@
  */
 package org.exoplatform.services.rest.impl.method;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.exoplatform.services.rest.BaseTest;
+import org.exoplatform.services.rest.impl.EnvironmentContext;
+import org.exoplatform.services.test.mock.MockHttpServletRequest;
+import org.exoplatform.services.test.mock.MockPrincipal;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.security.Principal;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.HttpMethod;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.SecurityContext;
 
 /**
  * Created by The eXo Platform SAS. <br>
@@ -77,9 +83,48 @@ public class OptionsMethodTest extends BaseTest
 
       Resource2 resource2 = new Resource2();
       registry(resource2);
-      assertEquals(200, launcher.service("OPTIONS", "/b", "", null, null, null).getStatus());
-      assertNotNull(launcher.service("OPTIONS", "/b", "", null, null, null).getResponse().getMetadata());
+      assertEquals(403, launcher.service("OPTIONS", "/b", "", null, null, null).getStatus());
 
+
+      EnvironmentContext envctx = new EnvironmentContext();
+      HttpServletRequest httpRequest = new MockHttpServletRequest("/b", null, 0, "OPTIONS", null);
+      envctx.put(HttpServletRequest.class, httpRequest);
+      envctx.put(SecurityContext.class, new MockSecurityContext("john"));
+
+      assertEquals(200, launcher.service("OPTIONS", "/b", "", null, null, envctx).getStatus());
+      assertNotNull(launcher.service("OPTIONS", "/b", "", null, null, envctx).getResponse().getMetadata());
+
+   }
+
+   protected static class MockSecurityContext implements SecurityContext {
+
+      private final String username;
+
+      public MockSecurityContext(String username) {
+         this.username = username;
+      }
+
+      public Principal getUserPrincipal() {
+         return new MockPrincipal(username);
+      }
+
+      public boolean isUserInRole(String role) {
+         if(username == null) {
+            return false;
+         }
+         if (role == null) {
+            return false;
+         }
+         return true;
+      }
+
+      public boolean isSecure() {
+         return false;
+      }
+
+      public String getAuthenticationScheme() {
+         return null;
+      }
    }
 
 }
